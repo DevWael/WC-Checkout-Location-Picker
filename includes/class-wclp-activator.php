d5773b59898863c099fc1c5ad25fef1568167305
Ahmad Wael <dev.ahmedwael@gmail.com>
2020-05-31 15:12:16 +0200

    initial files

---
+++ includes/class-wclp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/DevWael
+ * @since      1.0.0
+ *
+ * @package    Wclp
+ * @subpackage Wclp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wclp
+ * @subpackage Wclp/includes
+ * @author     Ahmad Wael <dev.ahmedwael@gmail.com>
+ */
+class Wclp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wclp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/DevWael
+ * @since      1.0.0
+ *
+ * @package    Wclp
+ * @subpackage Wclp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wclp
+ * @subpackage Wclp/includes
+ * @author     Ahmad Wael <dev.ahmedwael@gmail.com>
+ */
+class Wclp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
